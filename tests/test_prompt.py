cookiecutter/cookiecutter

add dependant questions

tests/test_prompt.py
@@ -87,6 +87,32 @@ class TestPrompt:
         cookiecutter_dict = prompt.prompt_for_config(context)
         assert cookiecutter_dict == context['cookiecutter']
 
+    @pytest.mark.parametrize(
+        'context, expected_dict',
+        [
+            (
+                {'cookiecutter': {'key?{{True}}': 'default'}},
+                {'key': 'default'},
+            ),
+            ({'cookiecutter': {'key?{{False}}': 'default'}}, {'key': 'default'}),
+        ],
+        ids=[
+            'true dependent question',
+            'false dependent question',
+        ],
+    )
+    def test_prompt_for_config_dependent_question(
+        self, monkeypatch, context, expected_dict
+    ):
+        """Verify `prompt_for_config` call `read_user_variable` on text request."""
+        monkeypatch.setattr(
+            'cookiecutter.prompt.read_user_variable',
+            lambda var, default: default,
+        )
+
+        cookiecutter_dict = prompt.prompt_for_config(context)
+        assert cookiecutter_dict == expected_dict
+
     def test_prompt_for_config_dict(self, monkeypatch):
         """Verify `prompt_for_config` call `read_user_variable` on dict request."""
         monkeypatch.setattr(
@@ -439,3 +465,73 @@ def test_undefined_variable(context):
     error = err.value
     assert error.message == "Unable to render variable 'foo'"
     assert error.context == context
+
+
+class TestParseQuestionExpression(object):
+    """Class to unite parse question expression prompt related tests."""
+
+    @pytest.mark.parametrize(
+        'cookiecutter_dict, key, actual_key, should_present_question',
+        [
+            (
+                {"is_storage": True},
+                "access_mode?{{is_storage}}",
+                "access_mode",
+                True,
+            ),
+            (
+                {"is_storage": False},
+                "access_mode?{{is_storage}}",
+                "access_mode",
+                False,
+            ),
+            (
+                {"queue": "kafka"},
+                "topic?{{queue=='kafka'}}",
+                "topic",
+                True,
+            ),
+            (
+                {"queue": "rabbit"},
+                "topic?{{queue=='kafka'}}",
+                "topic",
+                False,
+            ),
+        ],
+    )
+    def test_successful_parses(
+        self, cookiecutter_dict, key, actual_key, should_present_question
+    ):
+        """Verify successful parses of question."""
+        env = environment.StrictEnvironment()
+        context = None
+        assert (
+            actual_key,
+            should_present_question,
+        ) == prompt.parse_question_expression(context, cookiecutter_dict, env, key)
+
+    @pytest.mark.parametrize(
+        'cookiecutter_dict, key, actual_key, should_present_question',
+        [
+            (
+                {"is_storage": True},
+                "access_mode?{{is_storag}}",
+                "access_mode",
+                True,
+            ),
+            (
+                {"queue": "rabbit"},
+                "topic?{{queue==kafka'}}",
+                "topic",
+                False,
+            ),
+        ],
+    )
+    def test_failed_parses(
+        self, cookiecutter_dict, key, actual_key, should_present_question
+    ):
+        """Verify successful parses of question."""
+        env = environment.StrictEnvironment()
+        context = None
+        with pytest.raises(exceptions.InvalidBooleanExpression):
+            prompt.parse_question_expression(context, cookiecutter_dict, env, key)

cookieninja-generator/cookieninja

tests/test_prompt.py
@@ -87,6 +87,32 @@ class TestPrompt:
         cookiecutter_dict = prompt.prompt_for_config(context)
         assert cookiecutter_dict == context['cookiecutter']
 
+    @pytest.mark.parametrize(
+        'context, expected_dict',
+        [
+            (
+                {'cookiecutter': {'key?{{True}}': 'default'}},
+                {'key': 'default'},
+            ),
+            ({'cookiecutter': {'key?{{False}}': 'default'}}, {'key': 'default'}),
+        ],
+        ids=[
+            'true dependent question',
+            'false dependent question',
+        ],
+    )
+    def test_prompt_for_config_dependent_question(
+        self, monkeypatch, context, expected_dict
+    ):
+        """Verify `prompt_for_config` call `read_user_variable` on text request."""
+        monkeypatch.setattr(
+            'cookieninja.prompt.read_user_variable',
+            lambda var, default: default,
+        )
+
+        cookiecutter_dict = prompt.prompt_for_config(context)
+        assert cookiecutter_dict == expected_dict
+
     def test_prompt_for_config_dict(self, monkeypatch):
         """Verify `prompt_for_config` call `read_user_variable` on dict request."""
         monkeypatch.setattr(
@@ -466,3 +492,73 @@ def test_undefined_variable(context):
     error = err.value
     assert error.message == "Unable to render variable 'foo'"
     assert error.context == context
+
+
+class TestParseQuestionExpression(object):
+    """Class to unite parse question expression prompt related tests."""
+
+    @pytest.mark.parametrize(
+        'cookiecutter_dict, key, actual_key, should_present_question',
+        [
+            (
+                {"is_storage": True},
+                "access_mode?{{is_storage}}",
+                "access_mode",
+                True,
+            ),
+            (
+                {"is_storage": False},
+                "access_mode?{{is_storage}}",
+                "access_mode",
+                False,
+            ),
+            (
+                {"queue": "kafka"},
+                "topic?{{queue=='kafka'}}",
+                "topic",
+                True,
+            ),
+            (
+                {"queue": "rabbit"},
+                "topic?{{queue=='kafka'}}",
+                "topic",
+                False,
+            ),
+        ],
+    )
+    def test_successful_parses(
+        self, cookiecutter_dict, key, actual_key, should_present_question
+    ):
+        """Verify successful parses of question."""
+        env = environment.StrictEnvironment()
+        context = None
+        assert (
+            actual_key,
+            should_present_question,
+        ) == prompt.parse_question_expression(context, cookiecutter_dict, env, key)
+
+    @pytest.mark.parametrize(
+        'cookiecutter_dict, key, actual_key, should_present_question',
+        [
+            (
+                {"is_storage": True},
+                "access_mode?{{is_storag}}",
+                "access_mode",
+                True,
+            ),
+            (
+                {"queue": "rabbit"},
+                "topic?{{queue==kafka'}}",
+                "topic",
+                False,
+            ),
+        ],
+    )
+    def test_failed_parses(
+        self, cookiecutter_dict, key, actual_key, should_present_question
+    ):
+        """Verify successful parses of question."""
+        env = environment.StrictEnvironment()
+        context = None
+        with pytest.raises(exceptions.InvalidBooleanExpression):
+            prompt.parse_question_expression(context, cookiecutter_dict, env, key)