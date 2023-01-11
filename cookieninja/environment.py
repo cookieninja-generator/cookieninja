cookiecutter/cookiecutter

Inline jinja2-time extension code

cookiecutter/environment.py
@@ -26,8 +26,8 @@ class ExtensionLoaderMixin:
             'cookiecutter.extensions.JsonifyExtension',
             'cookiecutter.extensions.RandomStringExtension',
             'cookiecutter.extensions.SlugifyExtension',
+            'cookiecutter.extensions.TimeExtension',
             'cookiecutter.extensions.UUIDExtension',
-            'jinja2_time.TimeExtension',
         ]
         extensions = default_extensions + self._read_extensions(context)
 

tests/test_environment.py
@@ -18,8 +18,8 @@ def test_env_should_raise_for_unknown_extension():
 def test_env_should_come_with_default_extensions():
     """Verify default extensions loaded with StrictEnvironment."""
     env = StrictEnvironment(keep_trailing_newline=True)
-    assert 'jinja2_time.jinja2_time.TimeExtension' in env.extensions
     assert 'cookiecutter.extensions.JsonifyExtension' in env.extensions
     assert 'cookiecutter.extensions.RandomStringExtension' in env.extensions
     assert 'cookiecutter.extensions.SlugifyExtension' in env.extensions
+    assert 'cookiecutter.extensions.TimeExtension' in env.extensions
     assert 'cookiecutter.extensions.UUIDExtension' in env.extensions

cookieninja-generator/cookieninja

cookieninja/environment.py
@@ -26,8 +26,8 @@ class ExtensionLoaderMixin:
             'cookieninja.extensions.JsonifyExtension',
             'cookieninja.extensions.RandomStringExtension',
             'cookieninja.extensions.SlugifyExtension',
+            'cookieninja.extensions.TimeExtension',
             'cookieninja.extensions.UUIDExtension',
-            'jinja2_time.TimeExtension',
         ]
         extensions = default_extensions + self._read_extensions(context)
 

tests/test_environment.py
@@ -18,8 +18,8 @@ def test_env_should_raise_for_unknown_extension():
 def test_env_should_come_with_default_extensions():
     """Verify default extensions loaded with StrictEnvironment."""
     env = StrictEnvironment(keep_trailing_newline=True)
-    assert 'jinja2_time.jinja2_time.TimeExtension' in env.extensions
     assert 'cookieninja.extensions.JsonifyExtension' in env.extensions
     assert 'cookieninja.extensions.RandomStringExtension' in env.extensions
     assert 'cookieninja.extensions.SlugifyExtension' in env.extensions
+    assert 'cookieninja.extensions.TimeExtension' in env.extensions
     assert 'cookieninja.extensions.UUIDExtension' in env.extensions